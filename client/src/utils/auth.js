fe5eb095031c7bd297670179738ac494e26d4862
Nichole <nicholewilburn@gmail.com>
2023-10-01 13:20:55 -0500

    location error

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,21 +1,19 @@
-// use this to decode a token and get the user's information out of it
 import decode from 'jwt-decode';
 
-// create a new class to instantiate for a user
 class AuthService {
-  // get user data
+  constructor(history) {
+    this.history = history;
+  }
+
   getProfile() {
     return decode(this.getToken());
   }
 
-  // check if user's logged in
   loggedIn() {
-    // Checks if there is a saved token and it's still valid
     const token = this.getToken();
-    return !!token && !this.isTokenExpired(token); // handwaiving here
+    return !!token && !this.isTokenExpired(token);
   }
 
-  // check if token is expired
   isTokenExpired(token) {
     try {
       const decoded = decode(token);
@@ -28,22 +26,26 @@ class AuthService {
   }
 
   getToken() {
-    // Retrieves the user token from localStorage
     return localStorage.getItem('id_token');
   }
 
   login(idToken) {
-    // Saves user token to localStorage
     localStorage.setItem('id_token', idToken);
-    window.location.assign('/');
+    console.log('Login method called'); // Add a debugging statement
+    if (this.history) {
+      console.log('Redirecting to /'); // Add a debugging statement
+      this.history.push('/');
+    }
   }
 
   logout() {
-    // Clear user token and profile data from localStorage
     localStorage.removeItem('id_token');
-    // this will reload the page and reset the state of the application
-    window.location.assign('/');
+    console.log('Logout method called'); // Add a debugging statement
+    if (this.history) {
+      console.log('Redirecting to /'); // Add a debugging statement
+      this.history.push('/');
+    }
   }
 }
 
-export default new AuthService();
+export default AuthService;
